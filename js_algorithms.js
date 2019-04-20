1085fb535ee02c391e232da7dbb357fb413ad2d4
Isabella <43819679+isqbellq@users.noreply.github.com>
2019-04-20 15:24:05 +0200

    Update js_algorithms.js

--- js_algorithms.js
+++ js_algorithms.js
@@ -137,11 +137,11 @@ reverse('hi hello'); // olleh ih
 // or #2
 
 function reverse (str) {
-    if (str === "") {
-        return "";
-    } else {
-        return reverse(str.substr(1)) + str.charAt(0);
-    }
+      if (str === "") {
+          return "";
+      } else {
+          return reverse(str.substr(1)) + str.charAt(0);
+      }
 }
 
 reverse('hi hello'); // olleh ih
