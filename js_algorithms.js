4d4125a816996c4ac94e7e310999bdb3f0a751c2
Isabella <43819679+isqbellq@users.noreply.github.com>
2019-04-22 22:15:08 +0200

    Update js_algorithms.js

--- js_algorithms.js
+++ js_algorithms.js
@@ -124,12 +124,12 @@ sumPrimes(10); // 17
 /* Reverse a string */
 
 function reverse(str){
-    let reversed = '';
+      let reversed = '';
   
-    for(let i = str.length - 1; i >= 0; i--) {
-    reversed += str[i];
-    }
-    return reversed;
+      for(let i = str.length - 1; i >= 0; i--) {
+            reversed += str[i];
+      }
+      return reversed;
 }
 
 reverse('hi hello'); // olleh ih
