56c022a05ffeee2e35bc8c3c65612c8203476bf5
Isabella <43819679+isqbellq@users.noreply.github.com>
2019-04-23 20:45:26 +0200

    Update js_algorithms.js

--- js_algorithms.js
+++ js_algorithms.js
@@ -110,9 +110,10 @@ function sumPrimes(num) {
     let sum = 0;
 
     for (let i = 2; i <= num; i++) {
-		if (isPrime(i)) {
-                    sum += i;
-                } 
+		
+	  if (isPrime(i)) {
+                sum += i;
+          } 
     }  
     return sum;
 }
