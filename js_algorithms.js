e1b96c9dad5816c107529e09b16e5f3a7cbc4f52
Isabella <43819679+isqbellq@users.noreply.github.com>
2019-04-30 23:00:36 +0200

    Update js_algorithms.js

--- js_algorithms.js
+++ js_algorithms.js
@@ -215,13 +215,13 @@ steamrollArray([1, [2], [3, [[4]]]]); // 1,2,3,4
 /* Convert a string from binary code to English. */
 
 function binaryAgent(str) {
- let splitted = str.split(" ");
- let english = [];
+   let splitted = str.split(" ");
+   let english = [];
 
- for(let i = 0; i < splitted.length; i++) {
-   english.push(String.fromCharCode(parseInt(splitted[i], 2)));
-      }
-  return english.join("");
+   for(let i = 0; i < splitted.length; i++) {
+     english.push(String.fromCharCode(parseInt(splitted[i], 2)));
+        }
+    return english.join("");
 }
 
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"); // Aren't bonfires fun!?
